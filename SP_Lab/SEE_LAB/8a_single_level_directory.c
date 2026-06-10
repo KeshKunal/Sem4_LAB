8d9693c7cc4682df3673d64660eb6246d8b81f77
KeshKunal <infini2ude@gmail.com>
2026-06-10 21:39:49 +0530

    single level

--- SP_Lab/SEE_LAB/8a_single_level_directory.c
+++ SP_Lab/SEE_LAB/8a_single_level_directory.c
@@ -33,96 +33,54 @@ Execution: ./dir1
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_FILES 30
+#define MAX_FILES 50
+#define MAX_NAME_LEN 20
 
-typedef struct {
-    char name[30];
-} File;
-
-int searchFile(File files[], int fileCount, const char *name)
+int main()
 {
-    for (int i = 0; i < fileCount; i++) {
-        if (strcmp(files[i].name, name) == 0) {
-            return i;
-        }
+    char files[MAX_FILES][MAX_NAME_LEN];
+    int n, i, found = 0;
+    char search[MAX_NAME_LEN];
+
+    printf("Enter number of files: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter file %d name: ", i + 1);
+        scanf("%s", files[i]);
     }
-    return -1;
-}
 
-int main(void)
-{
-    File files[MAX_FILES];
-    int fileCount = 0;
-    int choice;
+    // Display structure
+    printf("\nSingle Level Directory Structure:\n\n");
+    printf("Root\n");
+    printf(" |\n");
 
-    while (1) {
-        printf("\nSingle-Level File Organization\n");
-        printf("1. Create file\n");
-        printf("2. Delete file\n");
-        printf("3. Search file\n");
-        printf("4. Display all files\n");
-        printf("5. Exit\n");
-        printf("Enter choice: ");
-        scanf("%d", &choice);
+    for(i = 0; i < n; i++)
+    {
+        if(i == n - 1)
+            printf(" `-- %s\n", files[i]);
+        else
+            printf(" +-- %s\n", files[i]);
+    }
 
-        if (choice == 1) {
-            if (fileCount >= MAX_FILES) {
-                printf("Directory full.\n");
-                continue;
-            }
+    // Search operation
+    printf("\nEnter file name to search: ");
+    scanf("%s", search);
 
-            char fname[30];
-            printf("Enter file name: ");
-            scanf("%s", fname);
-
-            if (searchFile(files, fileCount, fname) != -1) {
-                printf("File already exists.\n");
-            } else {
-                strcpy(files[fileCount].name, fname);
-                fileCount++;
-                printf("File created.\n");
-            }
-        } else if (choice == 2) {
-            char fname[30];
-            printf("Enter file name to delete: ");
-            scanf("%s", fname);
-
-            int pos = searchFile(files, fileCount, fname);
-            if (pos == -1) {
-                printf("File not found.\n");
-            } else {
-                for (int i = pos; i < fileCount - 1; i++) {
-                    files[i] = files[i + 1];
-                }
-                fileCount--;
-                printf("File deleted.\n");
-            }
-        } else if (choice == 3) {
-            char fname[30];
-            printf("Enter file name to search: ");
-            scanf("%s", fname);
-
-            int pos = searchFile(files, fileCount, fname);
-            if (pos == -1) {
-                printf("File not found.\n");
-            } else {
-                printf("File found at index %d in directory.\n", pos);
-            }
-        } else if (choice == 4) {
-            if (fileCount == 0) {
-                printf("No files in directory.\n");
-            } else {
-                printf("\nFiles in directory:\n");
-                for (int i = 0; i < fileCount; i++) {
-                    printf("%d. %s\n", i + 1, files[i].name);
-                }
-            }
-        } else if (choice == 5) {
+    for(i = 0; i < n; i++)
+    {
+        if(strcmp(files[i], search) == 0)
+        {
+            found = 1;
             break;
-        } else {
-            printf("Invalid choice.\n");
         }
     }
+
+    if(found)
+        printf("File '%s' found in Root directory.\n", search);
+    else
+        printf("File '%s' not found.\n", search);
 
     return 0;
 }
